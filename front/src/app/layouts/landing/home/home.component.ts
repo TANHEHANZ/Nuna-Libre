862cd53e2b9c81eb4e02eb75b3b04fbc6fe4b4d1
TANHEHANZ <hantach10@gmail.com>
2024-12-23 11:03:16 -0400

    configure home

--- front/src/app/layouts/landing/home/home.component.ts
+++ front/src/app/layouts/landing/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FuseLoadingBarComponent } from '../../../../@fuse/components/loading-bar';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule,FuseLoadingBarComponent],
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
